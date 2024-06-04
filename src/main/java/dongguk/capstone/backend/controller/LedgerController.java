cc1ef2418367c7a134830caa136161f46234aacd
zzammin <woalsdl7399@naver.com>
2024-06-04 19:40:11 +0900

    카테고리 분류 ec2 진짜 데이터

--- src/main/java/dongguk/capstone/backend/controller/LedgerController.java
+++ src/main/java/dongguk/capstone/backend/controller/LedgerController.java
@@ -63,35 +63,34 @@ public class LedgerController {
      * @param userId
      * @return
      */
-    @GetMapping("/logs")
-    public ResponseEntity<LogsResponseDTO> log(@RequestParam("userId") Long userId) {
-        // !!!!!!!!!!!!!!! 고치자 !!!!!!!!!!!!!!!
-        // 내가 보기엔 accountRegist 메소드를 호출할 당시에는 accountRegistRequestDTO에 값이 있지만,
-        // 그 이후에 log 메소드에서 사용할 때는 값이 적용되지 않아서 null 값이다.
-        // ※ accountRegistRequestDTO와 cardRegistRequestDTO는 각각 계좌 및 카드를 등록할 때 사용되는 요청 DTO이며, 메소드 호출 사이에 상태가 유지되지 않습니다. ※
-        // => 그래서 nullPointerException이 발생하는 듯..
-        // 값을 유지할 수 있는 방법이 없나? 아닌가? 동시성 문제가 있으려나?
-//        log.info("logs", accountRegistRequestDTO); // null
-//        log.info("logs", cardRegistRequestDTO); // null
-        // 아 여기서 accountRegistRequestDTO 이거랑 cardRegistRequestDTO 이거 쓰면 안되는건가?
-        LogsResponseDTO logsResponseDTO = ledgerService.log(userId);
-        // 만약 accountLogResponseDTO가 존재한다면, 200 ok 와 body로 accountLogsResponseDTO 전해주기
-//        if(accountLogsResponseDTO!=null)
-//            return ResponseEntity.ok().body(accountLogsResponseDTO);
-//        else
-//            return ResponseEntity.badRequest().build();
-        return ResponseEntity.ok(logsResponseDTO);
-
-        // 테스트 진행
-        // => 일단 계좌 정보가 들어간 Account가 있어야 함.
-        // 근데 일단 실패를 봐야 하니까 아무 Account나 임의로 안에 넣어두고
-        // logs 테스트 메소드 안에서만
-    }
+//    @GetMapping("/logs")
+//    public ResponseEntity<LogsResponseDTO> log(@RequestParam("userId") Long userId) {
+//        // !!!!!!!!!!!!!!! 고치자 !!!!!!!!!!!!!!!
+//        // 내가 보기엔 accountRegist 메소드를 호출할 당시에는 accountRegistRequestDTO에 값이 있지만,
+//        // 그 이후에 log 메소드에서 사용할 때는 값이 적용되지 않아서 null 값이다.
+//        // ※ accountRegistRequestDTO와 cardRegistRequestDTO는 각각 계좌 및 카드를 등록할 때 사용되는 요청 DTO이며, 메소드 호출 사이에 상태가 유지되지 않습니다. ※
+//        // => 그래서 nullPointerException이 발생하는 듯..
+//        // 값을 유지할 수 있는 방법이 없나? 아닌가? 동시성 문제가 있으려나?
+////        log.info("logs", accountRegistRequestDTO); // null
+////        log.info("logs", cardRegistRequestDTO); // null
+//        // 아 여기서 accountRegistRequestDTO 이거랑 cardRegistRequestDTO 이거 쓰면 안되는건가?
+//        LogsResponseDTO logsResponseDTO = ledgerService.log(userId);
+//        // 만약 accountLogResponseDTO가 존재한다면, 200 ok 와 body로 accountLogsResponseDTO 전해주기
+////        if(accountLogsResponseDTO!=null)
+////            return ResponseEntity.ok().body(accountLogsResponseDTO);
+////        else
+////            return ResponseEntity.badRequest().build();
+//        return ResponseEntity.ok(logsResponseDTO);
+//
+//        // 테스트 진행
+//        // => 일단 계좌 정보가 들어간 Account가 있어야 함.
+//        // 근데 일단 실패를 봐야 하니까 아무 Account나 임의로 안에 넣어두고
+//        // logs 테스트 메소드 안에서만
+//    }
 
     @GetMapping("/test")
     public LogsResponseDTO test(@RequestParam("userId") Long userId){
-//        ledgerService.fetchAndSaveLogs(); // 데베에 거래 내역 들어감
-        LogsResponseDTO logsResponseDTO = ledgerService.log(userId); // 데베에서 거래 내역 가져 오기
-        return logsResponseDTO;
+        ledgerService.fetchAndSaveLogs(); // 데베에 거래 내역 들어감
+        return ledgerService.log(userId);
     }
 }
