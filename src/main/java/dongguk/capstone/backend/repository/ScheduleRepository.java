f8e24baea89445026a6fbdcd424b443ecfc0a7d6
zzammin <woalsdl7399@naver.com>
2024-06-09 16:47:27 +0900

    스케쥴 수정..!

--- src/main/java/dongguk/capstone/backend/repository/ScheduleRepository.java
+++ src/main/java/dongguk/capstone/backend/repository/ScheduleRepository.java
@@ -2,12 +2,15 @@ package dongguk.capstone.backend.repository;
 
 import dongguk.capstone.backend.domain.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-    Long findMaxScheduleIdByUserId(Long userId);
+    @Query("SELECT MAX(s.scheduleEmbedded.scheduleId) FROM Schedule s WHERE s.scheduleEmbedded.userId = :userId")
+    Long findMaxScheduleIdByUserId(@Param("userId") Long userId);
 
     List<Schedule> findByScheduleEmbeddedUserId(Long userId);
 }
