2dbdc117522a34532d1aacf6f94948b7da107f45
zzammin <woalsdl7399@naver.com>
2024-06-09 15:57:49 +0900

    스케쥴 수정

--- src/main/java/dongguk/capstone/backend/domain/Schedule.java
+++ src/main/java/dongguk/capstone/backend/domain/Schedule.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 public class Schedule {
 
     @Id
-    @Column(name = "user_id", nullable = false)
+    @Column(name = "user_id")
     private Long userId; // 스케쥴에서 fk인 user_id를 사용하면 NOT NULL인데, 회원가입을 해서 user를 만들면 기본값이 없다는 오류가 발생한다.
     // schedule 테이블에 데이터를 삽입할 때 발생하는 오류는 기본 키 제약 조건과 외래 키 참조 때문에 발생할 가능성이 큽니다. schedule 테이블의 user_id 열은 user 테이블의 user_id 열을 참조하고 있으며, user 테이블의 user_id 열은 NOT NULL 및 AUTO_INCREMENT로 정의되어 있습니다. 이는 user 테이블에 새로운 사용자가 추가될 때 데이터베이스에 의해 자동으로 생성되며, NULL일 수 없음을 의미합니다.
 
@@ -27,8 +27,7 @@ public class Schedule {
     @Column(length = 100)
     private String day;
 
-    @OneToOne(mappedBy = "schedule")
-    @MapsId
-    @JoinColumn(name = "user_id", nullable = false)
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 }
